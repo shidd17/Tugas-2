825dade996a998b2ac62821e8f4a1d30001ae305
shiddd <shidqifadrian.r@gmail.com>
2024-03-29 20:53:55 +0700

    tugas

---
+++ CodeIgniter-3.1.13/application/views/biodata.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=<?= base_url('assets/css/bootstrap.css')?>>
+    <title>Shidqi Fadrian Rustiawan</title>
+</head>
+<body>
+<div class="card" style="width: 18rem;">
+  <img src="shidqi.jpeg"class="card-img-top" alt="BIODATA">
+  <div class="card-body">
+    <h5 class="card-title">Shidqi Fadrian Rustiawan</h5>
+    <p class="card-text">
+        Kelas        : 17.6A.26</p>
+    <p> Alamat       : Jl.Kemanggisan Pulo</p>
+    <p> Hobby        : Main Games</p>
+    <p> Cita-cita    : Programmer</p>
+    <p> Pekerjaan    : Goreng risol</p>
+
+    <a href="#" class="btn btn-primary">Biodata Shidqi</a>
+  </div>
+</div>
+</body>
+</html>
